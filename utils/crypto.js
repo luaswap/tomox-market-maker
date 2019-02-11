027d3d63dc1a46f4a312814d53886df56f00be68
Hoang Trinh <hoang.trinhj@gmail.com>
2019-02-11 17:39:09 +0700

    Add Some Helper Methods

---
+++ utils/crypto.js
@@ -0,0 +1,35 @@
+import { utils } from 'ethers'
+import { randInt } from './helpers'
+
+export const getOrderHash = order => {
+  return utils.solidityKeccak256(
+    [
+      'bytes',
+      'bytes',
+      'bytes',
+      'bytes',
+      'uint256',
+      'uint256',
+      'uint256',
+      'uint256',
+      'uint256',
+      'uint256',
+    ],
+    [
+      order.exchangeAddress,
+      order.userAddress,
+      order.baseToken,
+      order.quoteToken,
+      order.amount,
+      order.pricepoint,
+      order.side === 'BUY' ? '0' : '1',
+      order.nonce,
+      order.makeFee,
+      order.takeFee,
+    ],
+  )
+}
+
+export const getRandomNonce = () => {
+  return randInt(0, 1e16).toString()
+}
