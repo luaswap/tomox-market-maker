b0620b90c22860a28f5d981bf158b1b2e8da5cfe
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-08-22 07:52:31 +0000

    change to btc/tomo

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ export const BASE_URL = process.env.BASE_URL
 export const WS_BASE_URL = process.env.WS_BASE_URL
 
 export const tokenAddresses = {
-  TOMO: '0x0000000000000000000000000000000000000001',
-  ETH: process.env.ETH_ADDRESS,
+  TOMO: process.env.TOMO_ADDRESS,
+  BTC: process.env.BTC_ADDRESS
 }
 
 export const defaultOrderParams = {
@@ -15,6 +15,6 @@ export const defaultOrderParams = {
 
 export const minimumPriceStepChange = utils.bigNumberify(10).pow(36).div(utils.bigNumberify(1e2)) // 0.01
 
-export const exchangeAddress = '0xd68c26b99a40227C4AbE51020eDFd9BbA438B297'
+export const exchangeAddress = process.env.EX_COINBASE
 
 export const coinMarketCapAPI = 'https://pro-api.coinmarketcap.com'
