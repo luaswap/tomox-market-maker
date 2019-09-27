153fc1a12a4e1827cf5a6bee6049167819dda24a
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-09-27 02:41:43 +0000

    fix api

--- services/coingecko.js
+++ services/coingecko.js
@@ -7,11 +7,6 @@ export const getLatestPrice = async () => {
     console.log('Fetch data from coingecko')
     const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=tomochain&vs_currencies=btc`)
 
-    if (response.data.status.error_code !== 0) {
-      console.log(response.data.status.error_message)
-      return
-    }
-
     return 1/response.data.tomochain.btc
   } catch (err) {
     console.log(err)
