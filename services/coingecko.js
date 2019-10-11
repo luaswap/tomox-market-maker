4f110bf1659b0b2faf4a621ce78d467563620eea
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-10-11 10:30:05 +0000

    remove log

--- services/coingecko.js
+++ services/coingecko.js
@@ -2,7 +2,6 @@ const axios = require('axios')
 
 const getLatestPrice = async () => {
     try {
-        console.log('Fetch data from coingecko')
         const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=tomochain&vs_currencies=btc`)
 
         return 1/response.data.tomochain.btc
