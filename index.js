00fda46bcce08c3d0eae6482f60eccc250cfcf28
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-10-12 07:09:59 +0000

    bug amount

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const defaultAmount = 10000 // TOMO
 const minimumPriceStepChange = 1 // TOMO
 const FIXA = 1 // amount decimals
 const FIXP = 0 // price decimals
-const ORDERBOOK_LENGTH = 10 // number of order in orderbook
+const ORDERBOOK_LENGTH = 5 // number of order in orderbook
 
 const runMarketMaker = async () => {
     let hash = false
@@ -66,18 +66,26 @@ const runMarketMaker = async () => {
 }
 
 const handleEmptyOrderbook = async (side) => {
+    let ranNum = Math.floor(Math.random() * ORDERBOOK_LENGTH) + 1
     try {
         const latestPrice = await getLatestPrice()
         let amount = (defaultAmount/latestPrice).toFixed(FIXA).toString()
-        let price = (side === 'BUY' ? latestPrice - ORDERBOOK_LENGTH * minimumPriceStepChange : latestPrice + ORDERBOOK_LENGTH * minimumPriceStepChange).toFixed(FIXP)
-        let o = await tomox.createOrder({
-            baseToken: process.env.BTC_ADDRESS,
-            quoteToken: process.env.TOMO_ADDRESS,
-            price: price,
-            amount: amount,
-            side: side
+        let price = (side === 'BUY' ? latestPrice - (ORDERBOOK_LENGTH - 1) * minimumPriceStepChange : latestPrice + (ORDERBOOK_LENGTH - 1) * minimumPriceStepChange)
+        let orders = []
+        for (let i = 0; i < ORDERBOOK_LENGTH - 1; i++) {
+            let o = {
+                baseToken: process.env.BTC_ADDRESS,
+                quoteToken: process.env.TOMO_ADDRESS,
+                price: (price + (i * minimumPriceStepChange)).toFixed(FIXP),
+                amount: amount * ranNum,
+                side: side
+            }
+            orders.push(o)
+        }
+        let ret = await tomox.createManyOrders(orders)
+        orders.forEach((or, k) => {
+            console.log('BUY BTC/TOMO', or.price, or.amount, ret[k].nonce)
         })
-        console.log('BUY BTC/TOMO', price, amount, o.nonce)
     } catch (err) {
         console.log(err)
     }
@@ -90,8 +98,8 @@ const cancel = async (hash, nonce) => {
 }
 
 const match = async () => {
+    let ranNum = Math.floor(Math.random() * ORDERBOOK_LENGTH) + 1
     try {
-        let ranNum = Math.floor(Math.random() * ORDERBOOK_LENGTH) + 1
         const orderBookData = await getOrderBook(process.env.BTC_ADDRESS, process.env.TOMO_ADDRESS)
         if (!orderBookData) {
             process.exit(0)
@@ -102,7 +110,7 @@ const match = async () => {
             if (orderBookData.asks.length >= ORDERBOOK_LENGTH) {
                 const bestBid = orderBookData.asks[ORDERBOOK_LENGTH - 1]
                 const latestPrice = await getLatestPrice()
-                let amount = ((ranNum-1) * defaultAmount/latestPrice).toFixed(FIXA).toString()
+                let amount = (ranNum * defaultAmount/latestPrice).toFixed(FIXA).toString()
                 let price = (bestBid.pricepoint / 10 ** 18).toFixed(FIXP)
                 let side = 'BUY'
                 let o = await tomox.createOrder({
@@ -142,8 +150,9 @@ const match = async () => {
 
 
 const calculateBetterBid = (currentBestBid) => {
+    let ranNum = Math.floor(Math.random() * ORDERBOOK_LENGTH) + 1
     const newBidOrder = {
-        amount: (defaultAmount/(currentBestBid.pricepoint/1e+18)).toFixed(FIXA),
+        amount: (defaultAmount * ranNum/(currentBestBid.pricepoint/1e+18)).toFixed(FIXA),
         price: (new BigNumber(currentBestBid.pricepoint/1e+18).plus(minimumPriceStepChange)).toFixed(FIXP).toString()
     }
 
@@ -151,8 +160,9 @@ const calculateBetterBid = (currentBestBid) => {
 }
 
 const calculateBetterAsk = (currentBestAsk) => {
+    let ranNum = Math.floor(Math.random() * ORDERBOOK_LENGTH) + 1
     const newAskOrder = {
-        amount: (defaultAmount/(currentBestAsk.pricepoint/1e+18)).toFixed(FIXA),
+        amount: (defaultAmount * ranNum/(currentBestAsk.pricepoint/1e+18)).toFixed(FIXA),
         price: (new BigNumber(currentBestAsk.pricepoint/1e+18).minus(minimumPriceStepChange)).toFixed(FIXP).toString()
     }
 
