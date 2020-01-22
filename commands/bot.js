cecf3bfe3cef0ee6e92858af706b5b001d8b3e71
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-01-22 09:23:25 +0000

    add cal precision

--- commands/bot.js
+++ commands/bot.js
@@ -21,6 +21,9 @@ let sellPrices = []
 let buyPrices = []
 
 const createOrder = async (price, amount, side) => {
+    let prec = calcPrecision(price)
+    price = new BigNumber(price).toFixed(prec.pricePrecision)
+    amount = new BigNumber(amount).toFixed(prec.amountPrecision)
     let o = await tomox.createOrder({
         baseToken: baseToken,
         quoteToken: quoteToken,
@@ -223,6 +226,36 @@ const run = async (p) => {
         await runMarketMaker()
         await sleep(speed)
     }
+}
+
+calcPrecision = (price) => {
+    const totalPrecision = 8
+    let pricePrecision = 4
+    let amountPrecision = totalPrecision - pricePrecision
+    if (!price) return {pricePrecision: totalPrecision, amountPrecision: totalPrecision}
+    switch (true) {
+        case (price >= 50):
+            pricePrecision = 2
+            amountPrecision = totalPrecision - pricePrecision
+            break
+        case (price >= 1):
+            pricePrecision = 4
+            amountPrecision = totalPrecision - pricePrecision
+            break
+        case (price >= 0.1):
+            pricePrecision = 5
+            amountPrecision = totalPrecision - pricePrecision
+            break
+        case (price >= 0.001):
+            pricePrecision = 6
+            amountPrecision = totalPrecision - pricePrecision
+            break
+        default:
+            pricePrecision = 8
+            amountPrecision = totalPrecision - pricePrecision
+            break
+    }
+    return { pricePrecision, amountPrecision }
 }
 
 module.exports = { run }
